3b351163652539169ef7b916047062e149e7c470
Cloudtemplar26 <fredrichtan@icloud.com>
2026-04-08 17:13:45 -0400

    fixed angular animation for deployment

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(),
     provideRouter(routes),
-    provideAnimationsAsync()
+    provideAnimations()
   ]
 };
